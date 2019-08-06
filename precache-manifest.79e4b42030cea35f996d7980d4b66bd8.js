7bb9f9c37d25881abb795c8b3b6dfeb8eee499ae
Seunghoon Lee <kneeprayer@gmail.com>
2019-08-06 22:35:11 +0900

    Updates

---
+++ precache-manifest.79e4b42030cea35f996d7980d4b66bd8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8da7fad28ef8bd51f935afc0dc7a33",
+    "url": "/nomflix-handson/index.html"
+  },
+  {
+    "revision": "de97be28b148e0849144",
+    "url": "/nomflix-handson/static/js/2.90d3d86e.chunk.js"
+  },
+  {
+    "revision": "6ed839a6c90bb05afccd",
+    "url": "/nomflix-handson/static/js/main.a8925f95.chunk.js"
+  },
+  {
+    "revision": "42b082ef4a1a30076c28",
+    "url": "/nomflix-handson/static/js/runtime~main.a1b892ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nomflix-handson/precache-manifest.b6287b12e6034b2ce4695919449f836a.js"
+  "/nomflix-handson/precache-manifest.79e4b42030cea35f996d7980d4b66bd8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nomflix-handson/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nomflix-handson/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
