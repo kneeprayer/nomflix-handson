3e4fe97c6c86b3c8cc2e26651620fd0ea4514d89
Seunghoon Lee <kneeprayer@gmail.com>
2019-09-11 07:00:24 +0900

    bug fix Name & overview in Session tab

--- src/Components/Seasons.js
+++ src/Components/Seasons.js
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { tvApi } from "../api";
+
+const SeasonInfo = ({ name, path, overview }) => (
+  <>
+    <h1>Name : {`${name}`}</h1>
+    <img src={`${path}`} width="150px" />
+    <p>Overview : {`${overview}`}</p>
+  </>
+);
 
 const Seasons = ({ match }) => {
   const [seasons, setSeasons] = useState(null);
@@ -20,13 +29,22 @@ const Seasons = ({ match }) => {
   return (
     <div>
       {seasons &&
-        seasons.map(item => {
-          <h1>Name : {`${item.name}`}</h1>;
-          item.poster_path && <img src={`//image.tmdb.org/t/p/w300/${item.poster_path}`} width="150px" />;
-          <p>Overview : {`${item.overview}`}</p>;
-        })}
+        seasons.map(item => (
+          // eslint-disable-next-line react/jsx-key
+          <SeasonInfo
+            name={item.name}
+            path={`//image.tmdb.org/t/p/w300/${item.poster_path}`}
+            overview={item.overview}
+          />
+        ))}
     </div>
   );
+};
+
+Seasons.propTypes = {
+  name: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  overview: PropTypes.string.isRequired
 };
 
 export default Seasons;
